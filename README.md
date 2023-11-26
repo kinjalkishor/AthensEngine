d004147952a583ac279d21f17304f4a6ec67b932
Kinjal Kishor <79158943+kinjalkishor@users.noreply.github.com>
2023-11-26 21:49:39 +0530

    updated build

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # AthensEngine
 AthensEngine Game Engine  
+Start compilation from "AthensEngine\sample_game\source\code" using Visual Studio Solution (.sln) files:  
+COMPILE in RELEASE mode only  
+Athens Engine - test2.sln  
+Graphics init tutorials - test_zfx.sln  
 Change RS_MODEL_PATH, RS_SHADER_PATH, RS_MEDIA_PATH, TUT_MEDIA_PATH to folders containing media and shaders (not uploaded on github for now).  
 Set in "C/C++->General->Additional Include Directories" & "Linker->General->Additional Library Directories" the path "D:/osl" to folder containing "DXSDK_Jun10" (needed for d3d9b, d3d9c, d3d10 renderers).  
 
--- sample_game/source/code/test3_zgfx/rs_d3d11.h
+++ sample_game/source/code/test3_zgfx/rs_d3d11.h
@@ -1,44 +1,6 @@
 #pragma once
 
-//------------------------------------
-#include <d3d11.h>
-#include <d3d11_4.h>
-//#include <dxgi.h>
-#include <dxgi1_6.h>
-
-#pragma comment(lib, "d3d11.lib")
-#pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "dxguid.lib")
-// For using Windows Imaging Component (WIC)
-#pragma comment(lib, "uuid.lib")
-//#pragma comment(lib,"windowscodecs.lib")
-
-// for compiling shader files, D3DCompileFromFile
-#include <D3Dcompiler.h>
-//#include <D3Dcommon.h>
-
-#pragma comment(lib, "d3dcompiler.lib")
-
-
-// For D3D11TextureLoader
-////#include <wrl.h>
-//#include <wrl/client.h>
-//using Microsoft::WRL::ComPtr;
-// For RoInitializeWrapper required for CreateWICTextureFromFile
-//#pragma comment(lib, "runtimeobject.lib")
-
-//=====================================
-//#include "tut/d3dxlib/DirectXMath/Inc/DirectXMath.h"
-//#include "tut/d3dxlib/DirectXMath/Inc/DirectXPackedVector.h"
-//#include "tut/d3dxlib/DirectXMath/Inc/DirectXColors.h"
-//#include "tut/d3dxlib/DirectXMath/Inc/DirectXCollision.h"
-
-// For textures
-#include "d3dxlib/TextureLoader/DDSTextureLoader11.h"
-#include "d3dxlib/TextureLoader/WICTextureLoader11.h"
-// Add DDSTextureLoader11.cpp to project also
-//------------------------------------
-
+#include "rs_hdr_d3d11.h"
 
 #include "rs_irenderer.h"
 
